949399f74564506a8f29a86bb6deb091b22aa3d3
Jomiel Enriquez <77730490+jomielenriquez@users.noreply.github.com>
2022-12-03 01:52:26 +0000

    UI Update

--- src/App.js
+++ src/App.js
@@ -5,74 +5,74 @@ export default function App() {
     <div className="Calculator">
       <div className="display">
         <div className="firstLine">
-          <h3>First Line</h3>
+          <h3>1234+56789=</h3>
         </div>
         <div className="secondLine">
-          <h3>Second Line</h3>
+          <h3>9876543210</h3>
         </div>
       </div>
       <div className="buttonPad">
         <div className="buttonRow">
           <div className="buttonCell">
-            <button className="longButton">AC</button>
+            <button className="longButton AC">AC</button>
           </div>
           <div className="buttonCell">
-            <button>/</button>
+            <button className="Operator">/</button>
           </div>
           <div className="buttonCell">
-            <button>x</button>
+            <button className="Operator">x</button>
           </div>
         </div>
         <div className="buttonRow">
           <div className="buttonCell">
-            <button>7</button>
+            <button className="Num">7</button>
           </div>
           <div className="buttonCell">
-            <button>8</button>
+            <button className="Num">8</button>
           </div>
           <div className="buttonCell">
-            <button>9</button>
+            <button className="Num">9</button>
           </div>
           <div className="buttonCell">
-            <button>-</button>
+            <button className="Operator">-</button>
           </div>
 
           
         </div>
         <div className="buttonRow">
           <div className="buttonCell">
-            <button>4</button>
+            <button className="Num">4</button>
           </div>
           <div className="buttonCell">
-            <button>5</button>
+            <button className="Num">5</button>
           </div>
           <div className="buttonCell">
-            <button>6</button>
+            <button className="Num">6</button>
           </div>
           <div className="buttonCell">
-            <button>+</button>
-          </div>
-        </div>
-        <div className="buttonRow">
-          <div className="buttonCell">
-            <button>1</button>
-          </div>
-          <div className="buttonCell">
-            <button>2</button>
-          </div>
-          <div className="buttonCell">
-            <button>3</button>
-          </div>
-          <div className="buttonCell">
-            <button>=</button>
+            <button className="Operator">+</button>
           </div>
         </div>
         <div className="buttonRow">
           <div className="buttonCell">
-            <button className="longButton">0</button>
+            <button className="Num">1</button>
           </div>
           <div className="buttonCell">
-            <button className="longButton">.</button>
+            <button className="Num">2</button>
+          </div>
+          <div className="buttonCell">
+            <button className="Num">3</button>
+          </div>
+          <div className="buttonCell Equal">
+            <button className="Operator">=</button>
+          </div>
+        </div>
+        <div className="buttonRow">
+          <div className="buttonCell">
+            <button className="longButton Num">0</button>
+          </div>
+          <div className="buttonCell">
+            <button className="longButton Num">.</button>
           </div>
         </div>
       </div>
